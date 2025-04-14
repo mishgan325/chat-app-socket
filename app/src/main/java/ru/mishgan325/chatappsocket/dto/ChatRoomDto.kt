e9a568a30602b3de18eed62ba7c99a9186e4da58
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-15 00:30:24 +0300

    Update DTO

--- app/src/main/java/ru/mishgan325/chatappsocket/dto/ChatRoomDto.kt
+++ app/src/main/java/ru/mishgan325/chatappsocket/dto/ChatRoomDto.kt
@@ -2,17 +2,18 @@ package ru.mishgan325.chatappsocket.dto
 
 import com.google.gson.annotations.SerializedName
 import ru.mishgan325.chatappsocket.models.Chat
+import ru.mishgan325.chatappsocket.models.User
 
 data class ChatRoomDto(
     @SerializedName("id") val id: Long,
     @SerializedName("name") val name: String,
     @SerializedName("type") val type: String
-)
-
-fun ChatRoomDto.toChat(): Chat {
-    return Chat(
-        id = this.id,
-        name = this.name,
-        type = this.type
-    )
+) {
+    fun toChat(): Chat {
+        return Chat(
+            id = this.id,
+            name = this.name,
+            type = this.type
+        )
+    }
 }
--- app/src/main/java/ru/mishgan325/chatappsocket/dto/UserDto.kt
+++ app/src/main/java/ru/mishgan325/chatappsocket/dto/UserDto.kt
@@ -1,8 +1,17 @@
 package ru.mishgan325.chatappsocket.dto
 
 import com.google.gson.annotations.SerializedName
+import ru.mishgan325.chatappsocket.models.Chat
+import ru.mishgan325.chatappsocket.models.User
 
 data class UserDto(
     @SerializedName("id") val id: Long,
     @SerializedName("username") val username: String
-)
+) {
+    fun toUser(): User {
+        return User(
+            id = this.id,
+            username = this.username
+        )
+    }
+}
