897aa21132b4398edc37b04e9cb9c72623a5ee1f
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-14 21:49:47 +0300

    Create settings screen

---
+++ app/src/main/java/ru/mishgan325/chatappsocket/presentation/screens/SettingsScreen.kt
@@ -0,0 +1,106 @@
+package ru.mishgan325.chatappsocket.presentation.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import ru.mishgan325.chatappsocket.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreen(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.settings)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
+        },
+//        bottomBar = {
+//            NavigationBar {
+//                NavigationBarItem(
+//                    selected = false,
+//                    onClick = { navController.navigate("chat_list") },
+//                    icon = {
+//                        Icon(
+//                            imageVector = Icons.Default.Home,
+//                            contentDescription = stringResource(R.string.chats)
+//                        )
+//                    },
+//                    label = { Text(stringResource(R.string.chats)) }
+//                )
+//                NavigationBarItem(
+//                    selected = true,
+//                    onClick = { /* Уже на экране настроек */ },
+//                    icon = {
+//                        Icon(
+//                            imageVector = Icons.Default.Settings,
+//                            contentDescription = stringResource(R.string.settings)
+//                        )
+//                    },
+//                    label = { Text(stringResource(R.string.settings)) }
+//                )
+//            }
+//        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = {
+                        navController.navigate("login") {
+                            popUpTo(0)
+                        }
+                    },
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ExitToApp,
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text(text = stringResource(R.string.log_out))
+                }
+            }
+        }
+    }
+}
