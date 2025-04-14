866cc7d342d748fc3d210db98f8244e6f50f25e6
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-14 21:55:54 +0300

    Export TabBar to components

---
+++ app/src/main/java/ru/mishgan325/chatappsocket/presentation/components/TabView.kt
@@ -0,0 +1,49 @@
+package ru.mishgan325.chatappsocket.presentation.components
+
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavController
+import ru.mishgan325.chatappsocket.dto.TabBarItem
+
+@Composable
+fun TabView(tabBarItems: List<TabBarItem>, navController: NavController) {
+    var selectedTabIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar {
+        // looping over each tab to generate the views and navigation for each item
+        tabBarItems.forEachIndexed { index, tabBarItem ->
+            NavigationBarItem(
+                selected = selectedTabIndex == index,
+                onClick = {
+                    if (selectedTabIndex != index) {
+                        selectedTabIndex = index
+                        navController.navigate(tabBarItem.title) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                },
+                icon = {
+                    TabBarIconView(
+                        isSelected = selectedTabIndex == index,
+                        selectedIcon = tabBarItem.selectedIcon,
+                        unselectedIcon = tabBarItem.unselectedIcon,
+                        title = tabBarItem.title,
+                        badgeAmount = tabBarItem.badgeAmount
+                    )
+                },
+                label = {Text(tabBarItem.title)})
+        }
+    }
+}
