edb97d1df2649c9576b7595bbceb730f3e49d830
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-14 21:59:36 +0300

    Minor changes

--- app/src/main/java/ru/mishgan325/chatappsocket/presentation/MainActivity.kt
+++ app/src/main/java/ru/mishgan325/chatappsocket/presentation/MainActivity.kt
@@ -12,38 +12,26 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Badge
-import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.mishgan325.chatappsocket.dto.TabBarItem
+import ru.mishgan325.chatappsocket.presentation.components.TabView
 import ru.mishgan325.chatappsocket.presentation.screens.ChatListScreen
 import ru.mishgan325.chatappsocket.presentation.screens.LoginScreen
 import ru.mishgan325.chatappsocket.presentation.screens.RegisterScreen
-import ru.mishgan325.chatappsocket.presentation.screens.SelectUserScreen
 import ru.mishgan325.chatappsocket.presentation.screens.SettingsScreen
 import ru.mishgan325.chatappsocket.presentation.ui.theme.ChatappsocketTheme
 import ru.mishgan325.chatappsocket.utils.SessionManager
+import ru.mishgan325.chatappsocket.viewmodels.ChatListViewModel
 import ru.mishgan325.chatappsocket.viewmodels.LoginViewModel
 import ru.mishgan325.chatappsocket.viewmodels.RegisterViewModel
-import ru.mishgan325.chatappsocket.viewmodels.ChatListViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -67,10 +55,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             ChatappsocketTheme {
                 val navController = rememberNavController()
-                val currentDestination by navController.currentBackStackEntryFlow.collectAsState(initial = navController.currentBackStackEntry)
+                val currentDestination by navController.currentBackStackEntryFlow.collectAsState(
+                    initial = navController.currentBackStackEntry
+                )
 
-                val homeTab = TabBarItem(title = "Chats", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
-                val settingsTab = TabBarItem(title = "Settings", selectedIcon = Icons.Filled.Settings, unselectedIcon = Icons.Outlined.Settings)
+                val homeTab = TabBarItem(
+                    title = "Chats",
+                    selectedIcon = Icons.Filled.Home,
+                    unselectedIcon = Icons.Outlined.Home
+                )
+                val settingsTab = TabBarItem(
+                    title = "Settings",
+                    selectedIcon = Icons.Filled.Settings,
+                    unselectedIcon = Icons.Outlined.Settings
+                )
                 val tabBarItems = listOf(homeTab, settingsTab)
 
                 val bottomBarRoutes = listOf("Chats", "Settings")
@@ -86,7 +84,7 @@ class MainActivity : ComponentActivity() {
                     NavHost(
                         navController = navController,
                         startDestination = startDestination,
-                        modifier = Modifier.padding(bottom=innerPadding.calculateBottomPadding())
+                        modifier = Modifier.padding(bottom = innerPadding.calculateBottomPadding())
                     ) {
                         composable("Login") {
                             LoginScreen(navController, loginViewModel)
@@ -110,70 +108,6 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun TabView(tabBarItems: List<TabBarItem>, navController: NavController) {
-    var selectedTabIndex by rememberSaveable {
-        mutableStateOf(0)
-    }
-
-    NavigationBar {
-        // looping over each tab to generate the views and navigation for each item
-        tabBarItems.forEachIndexed { index, tabBarItem ->
-            NavigationBarItem(
-                selected = selectedTabIndex == index,
-                onClick = {
-                    if (selectedTabIndex != index) {
-                        selectedTabIndex = index
-                        navController.navigate(tabBarItem.title) {
-                            popUpTo(navController.graph.startDestinationId) {
-                                saveState = true
-                            }
-                            launchSingleTop = true
-                            restoreState = true
-                        }
-                    }
-                },
-                icon = {
-                    TabBarIconView(
-                        isSelected = selectedTabIndex == index,
-                        selectedIcon = tabBarItem.selectedIcon,
-                        unselectedIcon = tabBarItem.unselectedIcon,
-                        title = tabBarItem.title,
-                        badgeAmount = tabBarItem.badgeAmount
-                    )
-                },
-                label = {Text(tabBarItem.title)})
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TabBarIconView(
-    isSelected: Boolean,
-    selectedIcon: ImageVector,
-    unselectedIcon: ImageVector,
-    title: String,
-    badgeAmount: Int? = null
-) {
-    BadgedBox(badge = { TabBarBadgeView(badgeAmount) }) {
-        Icon(
-            imageVector = if (isSelected) {selectedIcon} else {unselectedIcon},
-            contentDescription = title
-        )
-    }
-}
-
-@Composable
-@OptIn(ExperimentalMaterial3Api::class)
-fun TabBarBadgeView(count: Int? = null) {
-    if (count != null) {
-        Badge {
-            Text(count.toString())
         }
     }
 }
