3b534a10c4ca51904aa6faa01d22f1756f40904e
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-16 00:34:45 +0300

    Update min SDK to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.mishgan325.chatappsocket"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
