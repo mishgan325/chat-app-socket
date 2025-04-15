3cb93a64fbaac20126d534a13e457acd96c09d4f
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-16 00:36:56 +0300

    Add Chat screen to route

--- app/src/main/java/ru/mishgan325/chatappsocket/presentation/navigation/Screen.kt
+++ app/src/main/java/ru/mishgan325/chatappsocket/presentation/navigation/Screen.kt
@@ -1,9 +1,15 @@
 package ru.mishgan325.chatappsocket.presentation.navigation
 
+import android.net.Uri
+
 sealed class Screen(val route: String) {
     object Login : Screen("Login")
     object Register : Screen("Register")
     object Chats : Screen("Chats")
     object Settings : Screen("Settings")
     object CreateNewChat : Screen("New chat")
+    object Chat : Screen("chat/{chatRoomId}/{chatName}") {
+        fun withArgs(chatRoomId: String, chatName: String): String =
+            "chat/$chatRoomId/${Uri.encode(chatName)}"
+    }
 }
