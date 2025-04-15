263bc7ff3c86267ff9598b9e4b31f64416928f5d
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-15 23:56:16 +0300

    Update color

--- app/src/main/java/ru/mishgan325/chatappsocket/presentation/components/ChatListItem.kt
+++ app/src/main/java/ru/mishgan325/chatappsocket/presentation/components/ChatListItem.kt
@@ -24,7 +24,7 @@ fun ChatListItem(
             .fillMaxWidth()
             .padding(horizontal = 8.dp),
         colors = CardDefaults.cardColors(
-//            containerColor = MaterialTheme.colorScheme.su,
+            containerColor = MaterialTheme.colorScheme.surface,
             contentColor = MaterialTheme.colorScheme.onSurface
         ),
         shape = MaterialTheme.shapes.medium,
