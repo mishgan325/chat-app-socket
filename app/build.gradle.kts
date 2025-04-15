7d5dc5e9fc5e560398b2c2df9997934748e939e9
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-15 22:59:23 +0300

    Update lib version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.0")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -102,4 +102,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.3.1")
 
     implementation("com.squareup:javapoet:1.13.0")
+
+    // Paging
+    implementation("androidx.paging:paging-runtime:3.2.1")
+    implementation("androidx.paging:paging-compose:3.2.1")
 }
