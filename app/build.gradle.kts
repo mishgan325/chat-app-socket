d956ab6487753394a99e256ae4a09be06d6493c4
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-15 21:10:46 +0300

    Add extended icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
 
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
