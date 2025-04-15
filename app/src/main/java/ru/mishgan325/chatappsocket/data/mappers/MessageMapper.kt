c10baa22f80def7b5d7a98c619bf8a378350ef37
Michael Shibut <79378430+mishgan325@users.noreply.github.com>
2025-04-16 00:36:12 +0300

    Create MessageMappers

---
+++ app/src/main/java/ru/mishgan325/chatappsocket/data/mappers/MessageMapper.kt
@@ -0,0 +1,28 @@
+package ru.mishgan325.chatappsocket.data.mappers
+
+import ru.mishgan325.chatappsocket.data.api.model.Content
+import ru.mishgan325.chatappsocket.dto.ChatMessageDto
+import ru.mishgan325.chatappsocket.dto.SenderDto
+import ru.mishgan325.chatappsocket.models.Message
+import ru.mishgan325.chatappsocket.models.Sender
+
+fun Content.toChatMessageDto(): ChatMessageDto {
+    return ChatMessageDto(
+        id = id,
+        sender = SenderDto(sender.id, sender.username),
+        content = content,
+        fileUrl = fileUrl,
+        timestamp = timestamp
+    )
+}
+
+fun ChatMessageDto.toMessage(userId: Long?): Message {
+    return Message(
+        id = id,
+        sender = Sender(sender.id, sender.username),
+        content = content,
+        fileUrl = fileUrl,
+        timestamp = timestamp,
+        isMine = userId == sender.id
+    )
+}
